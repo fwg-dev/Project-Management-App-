74f173110bbba3c75c78821b39cc887118e1f03b
Faith Gicheha <fwgicheh@gmail.com>
2020-09-04 23:45:11 -0700

    add user and project foreign keys

--- db/migrate/20200905055337_create_tasks.rb
+++ db/migrate/20200905055337_create_tasks.rb
@@ -4,6 +4,8 @@ class CreateTasks < ActiveRecord::Migration[6.0]
       t.string :description
       t.date :schedule
       t.string :completion_status
+      t.belongs_to :project, foreign_key: true
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
