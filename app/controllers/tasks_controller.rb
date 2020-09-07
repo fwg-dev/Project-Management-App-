34780b8c77158d4a15baf9540cbb31e541d5042f
Faith Gicheha <fwgicheh@gmail.com>
2020-09-06 23:23:38 -0700

    add create method and params method)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,27 @@
 class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+    @task.build_project
+  end
+
+  def create 
+    @task = Task.new(task_params)
+    @task.user_id = session[:user_id]
+    if @task.save 
+     redirect_to task_path(@task)
+    else
+     render :new
+    end
+
+  end 
+
+  private
+
+  def task_params 
+    params.require(:task).permit(:description, :schedule, :completion_status, :project_id, project_attributes: [:name])
+  end 
+
+
 end
+
