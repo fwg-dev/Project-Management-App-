91c2e8b0e7e7355ecfed8ee93bb7911bed42ee10
Faith Gicheha <fwgicheh@gmail.com>
2020-09-09 00:03:56 -0700

    add edit and update methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,17 @@
 class TasksController < ApplicationController
 
   def new
-    @task = Task.new
+   # very simple code to create an empty task and send the user
+   # to the New view for it (new.html.erb), which will have a
+   # form for creating the task
+    
+   @task = Task.new
     @task.build_project
   end
+
+  def edit 
+    @task =Task.find(params[:id])
+  end 
 
   def create 
     @task = Task.new(task_params)
@@ -11,21 +19,37 @@ class TasksController < ApplicationController
     if @task.save 
      redirect_to task_path(@task)
     else
+      @task.build_project
      render :new
     end
 
   end 
 
-  def index
-    @tasks = Task.all.includes(:project)
+  def update 
+  end 
+  
+  def show
+     @task = Task.find_by_id(params[:id])
+    # redirect_to tasks_path if !@task
   end
+
+  
+  def index
+
+    @tasks = Task.all 
+  #   @project = Project.find_by_id(params[:id])
+  #   @task = @project.build_task
+
+  #   # task belongs to a project 
+  #   # @tasks = Task.all.includes(:project)
+
+  end
+
 
   private
 
   def task_params 
     params.require(:task).permit(:description, :schedule, :completion_status, :project_id, project_attributes: [:name])
   end 
-
-
 end
 
