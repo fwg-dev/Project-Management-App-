42978195cf18a354ba68daa9b6c2a014e1ae33c6
Faith Gicheha <fwgicheh@gmail.com>
2020-09-09 16:54:28 -0700

    modify all CRUD methods

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,57 @@
 class ProjectsController < ApplicationController
-
-  def new 
-  end 
   
-  def index 
-    @projects = Project.all
+  def index
+    @projects = Project.all 
   end
-  
-  
+
+  def show
+    @project = Project.find_by_id(params[:id])
+   # redirect_to project_path if !@project
+  end
+
+  def new
+   # code to create an empty project and send the user
+   # to the New view for it (new.html.erb), which will have a
+   # form for creating the project
+    
+   @project = Project.new
+  end
+
+  def edit 
+    @project =Project.find_by_id(params[:id])
+  end 
+
+  def create 
+    @project = Project.new(project_params)
+    @project.user_id = session[:user_id]
+    if @project.save
+     redirect_to project_path(@project)
+    else
+     render :new
+    end
+
+  end 
+
+  def update 
+    @project = Project.find(params[:id])
+    
+    if @project.update(project_params)
+      redirect_to @project 
+    else 
+      render 'edit'
+    end 
+  end 
+
+  def destroy 
+    @project = Project.find_by(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end 
+
+  private
+
+  def project_params 
+    params.require(:project).permit(:name, :completion_status)
+  end 
 end
+
