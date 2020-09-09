9b58252794283e6610b8ecb21117f175d102d2ae
Faith Gicheha <fwgicheh@gmail.com>
2020-09-09 16:38:59 -0700

    add completed status attribute

--- db/migrate/20200905054103_create_projects.rb
+++ db/migrate/20200905054103_create_projects.rb
@@ -2,6 +2,8 @@ class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
       t.string :name
+      t.boolean :completion_status
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
