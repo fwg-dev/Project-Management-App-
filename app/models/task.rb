f24f92fe77a1b6f7934320a17fac66c368e06d1f
Faith Gicheha <fwgicheh@gmail.com>
2020-09-11 12:43:40 -0700

    add schedule date validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,8 +4,19 @@ class Task < ApplicationRecord
 
 
   validates :description, presence: true
-  validates :schedule, presence: true
   validates :completion_status, presence: true
+  validates :schedule, presence: true
+  validate :schedule_date_cannot_be_in_the_past 
+ 
+
+    def schedule_date_cannot_be_in_the_past
+      #if shedule_date is past - throw error
+      if schedule.present? && schedule < Date.today
+        errors.add(:schedule_date, "can't be in the past")
+      end
+    end
+
+   
 
 
 end
