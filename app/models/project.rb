154630a42531a5b3b7b787b71199dca737587dd1
Faith Gicheha <fwgicheh@gmail.com>
2020-09-11 01:36:30 -0700

    added validation for nested route

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
   belongs_to :user #the creator 
   has_many :tasks
   has_many :users, through: :tasks
-
+# the point of the line above this is to be able to call .users on a project instance, to list all users who have tasks for the project
+  accepts_nested_attributes_for :tasks
   validates :name, presence: true
 end
