18a7f65c2641b62fc30e54eaada7e6932f006cfa
Faith Gicheha <fwgicheh@gmail.com>
2020-09-11 01:35:38 -0700

    add has_many created_projects association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :tasks
+  has_many :created_projects, foreign_key: "user_id", class_name: "Project"
   has_many :projects, through: :tasks
+# the point of the line above this is to be able to call .projects on a user instance, to list all projects they have tasks for
 
   has_secure_password 
 
