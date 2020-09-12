824a3451a0786f91dba9f6cc6a9c5afb295d84a3
Faith Gicheha <fwgicheh@gmail.com>
2020-09-11 21:16:46 -0700

    add taks display header helpers

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,11 @@
 module TasksHelper
+
+  def display_header(task)
+    if params[:project_id]
+      content_tag(:h1, "Create a new task for #{task.project.name} ")
+    else
+        content_tag(:h1, "Create a New Task")
+    end
+  end
+
 end
