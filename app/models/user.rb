81a773e246131db72ead3a7542e1ca06ce6fe8ed
Faith Gicheha <fwgicheh@gmail.com>
2020-09-13 05:11:59 -0700

    add google onmiauth scope method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,26 @@ class User < ApplicationRecord
   has_many :projects, through: :tasks
 # the point of the line above this is to be able to call .projects on a user instance, to list all projects they have tasks for
 
+validates :username, uniqueness: true,  presence: true
+# validates :email, presence: true
+
   has_secure_password 
 
-  validates :username, uniqueness: true,  presence: true
-  validates :email, presence: true
+
+  def self.find_or_create_with_oauth(auth)
+    u = User.find_by_uid(auth["uid"]) || User.find_or_create_by_email(auth)
+    u.email = auth["info"]["email"]
+    u.uid = auth["uid"]
+    #  binding.pry
+    u.save
+    u
+  end
+
+  def self.find_or_create_by_email(auth)
+    User.find_or_create_by(email: auth["info"]["email"]) do |u|
+      u.uid = auth["uid"]
+      u.password = SecureRandom.hex(20)
+      u.username = auth["info"]["name"]
+    end
+  end
 end
