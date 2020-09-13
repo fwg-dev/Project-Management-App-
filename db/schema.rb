b1c3dea522cec874c2aedaa0d174e7b7d83fd739
Faith Gicheha <fwgicheh@gmail.com>
2020-09-13 04:57:50 -0700

    add uid to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_055337) do
+ActiveRecord::Schema.define(version: 2020_09_13_093212) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_09_05_055337) do
     t.string "username"
     t.string "password_digest"
     t.string "email"
+    t.string "uid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
