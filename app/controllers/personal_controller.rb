cd76c8df7d219c667e4dec7e28659e666d6a5743
Faith Gicheha <fwgicheh@gmail.com>
2020-09-15 19:06:56 -0700

    add index page

---
+++ app/controllers/personal_controller.rb
@@ -0,0 +1,8 @@
+class PersonalController < ApplicationController
+
+  # def index 
+  #   @projects = current_user.projects
+  #   # @user = User.includes(project: :task).find_by_id(params[:id])
+  # end
+
+end
