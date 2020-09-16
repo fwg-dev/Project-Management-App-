68c9d38cd678cf2362ea85aaec26fdf1a18e1662
Faith Gicheha <fwgicheh@gmail.com>
2020-09-15 18:54:03 -0700

    add my_projects routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/' => 'sessions#welcome'
+   get '/' => 'sessions#welcome'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
@@ -11,11 +11,15 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
 
   get '/auth/google_oauth2/callback', to: 'sessions#google'
+
+  resources :my_projects, only: [:new, :create, :update, :index]
+
   
   resources :tasks
   resources :projects do 
     resources :tasks, only: [:new, :create, :index]
   end
+  
   resources :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
