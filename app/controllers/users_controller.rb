99b691ac727bbc3c6dadbdb2387c48adb71f4e64
Faith Gicheha <fwgicheh@gmail.com>
2020-09-15 19:33:34 -0700

    modify show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,25 @@
 class UsersController < ApplicationController
 
   def new 
-    @user = User.new
+     @user = User.new
   end
 
   def create 
     @user = User.new(user_params)
      if @user.save 
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to @user
+      # flash[:message] ="You've successfully created an account!"
      else
       render :new
+      # flash[:error] = "Account creation failure: #{@user.errors.full_messages.to_sentence}" 
      end
   end
 
   def show
+    redirect_if_not_logged_in
     @user = User.find_by_id(params[:id])
+    # @user = User.includes(projects: :task).find_by_id(params[:id])
      redirect_to '/' if !@user
   end
 
