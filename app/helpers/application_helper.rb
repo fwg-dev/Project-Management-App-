eb318071e028dd6ab58584629c7c6d35d629ab9d
Faith Gicheha <fwgicheh@gmail.com>
2020-09-15 19:32:40 -0700

    render nav bar and add alert class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def render_nav_bar
+    if logged_in?
+      render partial: 'layouts/loggedin_links'
+    else
+      render partial: 'layouts/loggedout_links'
+    end
+  end
+
+  def alert_class(flash_type)
+    case flash_type.to_sym 
+    when :notice 
+      "alert-success"
+    when :alert 
+      "alert-warning"
+    when :error 
+      "alert-danger"
+    end
+  end
 end
